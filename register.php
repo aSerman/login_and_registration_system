3757780ad5039f4168d9cedfd1277ee82a2520ca
Agnes Serman <agnesserman@outlook.com>
2015-08-01 23:17:31 +0000

    created a form to handle new user registrations

---
+++ register.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+if(isset($_SESSION['user'])!="")
+{
+ header("Location: home.php");
+}
+include_once 'dbconnect.php';
+
+if(isset($_POST['btn-signup']))
+{
+ $uname = mysql_real_escape_string($_POST['uname']);
+ $email = mysql_real_escape_string($_POST['email']);
+ $upass = md5(mysql_real_escape_string($_POST['pass']));
+ 
+ if(mysql_query("INSERT INTO users(username,email,password) VALUES('$uname','$email','$upass')"))
+ {
+  ?>
+        <script>alert('successfully registered ');</script>
+        <?php
+ }
+ else
+ {
+  ?>
+        <script>alert('error while registering you...');</script>
+        <?php
+ }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login & Registration System</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+
+</head>
+<body>
+<center>
+<div id="login-form">
+<form method="post">
+<table align="center" width="30%" border="0">
+<tr>
+<td><input type="text" name="uname" placeholder="Username" required /></td>
+</tr>
+<tr>
+<td><input type="email" name="email" placeholder="Email" required /></td>
+</tr>
+<tr>
+<td><input type="password" name="pass" placeholder="Password" required /></td>
+</tr>
+<tr>
+<td><button type="submit" name="btn-signup">Register</button></td>
+</tr>
+<tr>
+<td><a href="index.php">Already a registered ? Sign in here.</a></td>
+</tr>
+</table>
+</form>
+</div>
+</center>
+</body>
+</html>
